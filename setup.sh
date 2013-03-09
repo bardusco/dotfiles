7c065ff6980df44fd3cebf517cffcc9dc6b364ff
Danilo Bardusco <danilo@bardusco.com>
2013-03-09 22:36:46 +0000

    added setup

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+homesick=`wich homesick`
+
+if [ alias homesick 2>/dev/null >/dev/null ] then
+
+	homesick clone git://github.com/bardusco/dotfiles.git && homesick symlink dotfiles
+
+	cd ~/.homesick/repos/dotfiles/.git/hooks && ln -s ../../git_hooks/post_chekout
+
+	git submodules update
+else:
+	echo "Homesick not found!!"
+fi
