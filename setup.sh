7f37e6dcafb9cfb797a24f99352f40850e1897c2
Danilo Bardusco <danilo@bardusco.com>
2013-03-09 22:41:58 +0000

    fixed typo

--- setup.sh
+++ setup.sh
@@ -2,12 +2,10 @@
 
 homesick=`wich homesick`
 
-if [ alias homesick 2>/dev/null >/dev/null ] then
-
+if [ alias homesick 2>/dev/null >/dev/null ]
+then
 	homesick clone git://github.com/bardusco/dotfiles.git && homesick symlink dotfiles
-
 	cd ~/.homesick/repos/dotfiles/.git/hooks && ln -s ../../git_hooks/post_chekout
-
 	git submodules update
 else:
 	echo "Homesick not found!!"
